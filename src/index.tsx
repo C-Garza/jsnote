2b9a399fbb718a052e6b055e33771a0927ae5fa5
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-10-15 04:47:24 -0700

    Set up initial esbuild browser bundler plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,51 @@
+import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
+import { useState, useEffect, useRef } from "react";
+import { unpkgPathPlugin } from "./plugins/unpkg-path";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm"
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if(!ref.current) return;
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": "'production'",
+        global: "window"
+      }
+    });
+
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
+  };
+
   return (
-    <div>HI!</div>
+    <div>
+      <textarea value={input} onChange={e => setInput(e.target.value)}></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
   );
 };
 
