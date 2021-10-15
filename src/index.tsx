c5066acb9392baebb5f64be8fb1391e7bd650194
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-10-15 05:08:44 -0700

    Split plugins into separate plugins

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from "esbuild-wasm";
 import ReactDOM from "react-dom";
 import { useState, useEffect, useRef } from "react";
 import { unpkgPathPlugin } from "./plugins/unpkg-path";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const [input, setInput] = useState("");
@@ -26,14 +27,15 @@ const App = () => {
       entryPoints: ["index.js"],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         "process.env.NODE_ENV": "'production'",
         global: "window"
       }
     });
-
-    // console.log(result);
 
     setCode(result.outputFiles[0].text);
   };
