b7fb8dcdbbfa223930cadb15374fad20789ae444
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-11-04 03:39:41 -0700

    Add serve command to cli package

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function () {
-    console.log("Server is listening");
-});
+exports.serve = void 0;
+var serve = function (port, filename, dir) {
+    console.log("Serving traffic on port: ", port);
+    console.log("Saving/fetching cells from: ", filename);
+    console.log("File is in dir: ", dir);
+};
+exports.serve = serve;
