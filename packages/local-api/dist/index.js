d7fa9bec241366df70c75a89a20a667318420274
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-11-04 04:54:12 -0700

    Set up express to serve react build files

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,8 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
-var serve = function (port, filename, dir) {
+var path_1 = __importDefault(require("path"));
+var http_proxy_middleware_1 = require("http-proxy-middleware");
+var serve = function (port, filename, dir, useProxy) {
     var app = (0, express_1.default)();
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)({
+            target: "http://localhost:3000",
+            ws: true,
+            logLevel: "silent"
+        }));
+    }
+    else {
+        var packagePath = require.resolve("local-client/build/index.html");
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise(function (resolve, reject) {
         app.listen(port, function () {
             console.log("Listening on port:", port);
