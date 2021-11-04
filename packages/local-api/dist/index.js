e6422bf19f016d057006e493318d7b407d36e974
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2021-11-04 04:14:07 -0700

    Fix up error when port is in use

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -7,8 +7,11 @@ exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
 var serve = function (port, filename, dir) {
     var app = (0, express_1.default)();
-    app.listen(port, function () {
-        console.log("Listening on port: ", port);
+    return new Promise(function (resolve, reject) {
+        app.listen(port, function () {
+            console.log("Listening on port:", port);
+            resolve();
+        }).on("error", reject);
     });
 };
 exports.serve = serve;
